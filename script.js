2d1fde03575a1b7fd34e187b692c93bdef0a3646
lcmongwe <lcmongwe@gmail.com>
2022-03-11 20:17:00 +0300

    an alert of message recivied added to the form

---
+++ script.js
@@ -0,0 +1,9 @@
+function formReset() {
+  document.getElementById("form_data").reset();
+}
+
+function akhanNames() {
+  alert("thanks for your feedback your message was recived");
+
+  formReset();
+}
