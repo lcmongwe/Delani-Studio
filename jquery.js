f7dfdaabc30ea7be7070d2993df67b53a8881282
lcmongwe <lcmongwe@gmail.com>
2022-03-14 15:17:33 +0300

    what we do description text aligned to the centre

--- jquery.js
+++ jquery.js
@@ -1,36 +1,3 @@
-// $(document).ready(function () {
-//   $("#dess").mouseover(function () {
-//     $(".show").show();
-//     $("#des").hide();
-//   });
-//   $("#dess").mouseout(function () {
-//     $(".show").hide();
-//     $("#des").show();
-//   });
-// });
-
-// $(document).ready(function () {
-//   $("#devv").mouseover(function () {
-//     $(".show").show();
-//     $("#dev").hide();
-//   });
-//   $("#devv").mouseout(function () {
-//     $(".show").hide();
-//     $("#dev").show();
-//   });
-// });
-
-// $(document).ready(function () {
-//   $("#prodd").mouseover(function () {
-//     $(".show").show();
-//     $("#prod").hide();
-//   });
-//   $("#prodd").mouseout(function () {
-//     $(".show").hide();
-//     $("prod").show();
-//   });
-// });
-
 $(document).ready(function () {
   $("#des").click(function () {
     $("#des").hide();
